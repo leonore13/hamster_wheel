9e66b7d20bd68ee44b07f61156d26febc0c30e56
Sophia Nguyen <shinjitsu13@gmail.com>
2016-07-06 20:57:07 -0700

    Add files via upload

---
+++ Practice problems/src/Main.java
@@ -0,0 +1,54 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by sophianguyen on 2016-07-06.
+ */
+public class Main {
+    // A function that reverses the vowels of a string
+    // Ex. apple -> eppla; initial -> anitiil; practice -> pretica
+
+    public String reverseVowels(String word) {
+        List<Character> cword = new ArrayList<>();
+        for (char w : word.toCharArray()) {
+            cword.add(w);
+        }
+
+        List<Character> vowels = new ArrayList<>();
+        for (char c : "aeiou".toCharArray()) {
+            vowels.add(c);
+        }
+
+        int i = 0;
+        int j = cword.size()-1;
+
+        while (i < j) {
+            if (!vowels.contains(cword.get(i))) {
+                i++;
+                continue;
+            }
+
+            if (!vowels.contains(cword.get(j))) {
+                j--;
+                continue;
+            }
+
+            Character x = cword.get(i);
+            cword.set(i, cword.get(j));
+            cword.set(j, x);
+
+            i++;
+            j--;
+        }
+
+        return cword.toString();
+    }
+
+    public static void main(String[] args) {
+        Main main = new Main();
+
+        String print = main.reverseVowels("practice");
+
+        System.out.println(print);
+    }
+}
