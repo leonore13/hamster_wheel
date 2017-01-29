254362381a8404a3aee68813ba89271038851077
Sophia Nguyen <shinjitsu13@gmail.com>
2017-01-29 01:24:55 -0800

    A web-based timesheet entry form

---
+++ timesheet/timesheet-proc.php
@@ -0,0 +1,18 @@
+<?php
+
+if (empty($_POST['submit']))
+	{
+		echo "Form is not submitted";
+		exit;
+	}
+if (empty($_POST["email"]) || empty($_POST["time"]))
+	{
+		echo "Please fill the form"
+		exit;
+	}
+	
+$email = $_POST("email");
+$time = $_POST("time");
+
+
+?>
