468aeba725b7772820c335f20647a19364e8dc21
Sophia Nguyen <sophia.nguyen@alumni.ubc.ca>
2019-05-19 01:24:05 -0700

    iterative solution to twosums

--- Practice problems/twoSum.js
+++ Practice problems/twoSum.js
@@ -15,8 +15,30 @@ return [0, 1]. **/
  * @param {number} target
  * @return {number[]}
  */
+ // Iterative solution
 var twoSum = function(nums, target) {
-  return (1 + 6);
+  let temp = 0;
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = i+1; j < nums.length; j++) {
+      temp = nums[i] + nums[j];
+      if (temp === target) {
+        let solution = [i, j];
+        return solution;
+      }
+    }
+  }
+  return ("There are no numbers that sum to the target");
 };
 
-console.log(twoSum([], 1));
+console.log(twoSum([1, 2, 3, 4], 7));
+console.log(twoSum([1, 2, 3, 4], 10));
+
+// Recursive solution
+var recurTwoSum = function(nums, target) {
+  // TODO
+}
+
+// Memoized solution
+var memoTwoSum = function(nums, target, memo) {
+  // TODO
+}
